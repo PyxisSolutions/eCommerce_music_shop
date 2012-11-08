f45a3c91c1617e25714d8b44c2f49f8d6f238212
StevenButter <sbutter@dundee.ac.uk>
2012-11-08 19:10:28 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ECommerceMusicShop::Application.config.secret_token = 'f9c9d415710c909b6411a8be65a4c199f966642e19a479d74f3ea246b4579af7626eb9b379909d0dcf806222f5e2b91e38de86999e8126cc7ed3e98dcd0ad288'
