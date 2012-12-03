4be11b6be06689aa6fed49e7d698fc1888210e2a
StevenButter <sbutter@dundee.ac.uk>
2012-12-03 15:50:58 +0000

    default data for database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,117 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+["standard_user", "band", "admin"].each do |role|
+	Role.find_or_create_by_name(role)
+end
+
+r = Right.new
+r.name = "index users"
+r.controller = "users"
+r.action = "index"
+r.save
+
+r = Right.new
+r.name = "delete users"
+r.controller = "users"
+r.action = "delete"
+r.save
+
+r = Right.new
+r.name = "delete bands"
+r.controller = "bands"
+r.action = "delete"
+r.save
+
+r = Right.new
+r.name = "index bands"
+r.controller = "bands"
+r.action = "index"
+r.save
+
+r = Right.new
+r.name = "show bands"
+r.controller = "bands"
+r.action = "show"
+r.save
+
+r = Right.new
+r.name = "new tracks"
+r.controller = "tracks"
+r.action = "new"
+r.save
+
+r = Right.new
+r.name = "show tracks"
+r.controller = "tracks"
+r.action = "show"
+r.save
+
+r = Right.new
+r.name = "create tracks"
+r.controller = "tracks"
+r.action = "create"
+r.save
+
+r = Right.new
+r.name = "download tracks"
+r.controller = "tracks"
+r.action = "download"
+r.save
+
+r = Right.new
+r.name = "create line_items"
+r.controller = "line_items"
+r.action = "create"
+r.save
+
+r = Right.new
+r.name = "show cart"
+r.controller = "carts"
+r.action = "show"
+r.save
+
+r = Right.new
+r.name = "paypal express checkout"
+r.controller = "paypal_express"
+r.action = "checkout"
+r.save
+
+r = Right.new
+r.name = "confirm purchase"
+r.controller = "paypal_express"
+r.action = "confirm"
+r.save
+
+r = Right.new
+r.name = "take payment"
+r.controller = "paypal_express"
+r.action = "purchase"
+r.save
+
+r = Right.new
+r.name = "show users"
+r.controller = "users"
+r.action = "show"
+r.save
+
+r = Right.new
+r.name = "view purchases"
+r.controller = "purchases"
+r.action = "index"
+r.save
+
+[4, 5, 7, 9, 10, 11, 12, 13, 14, 15, 16].each do |i|
+	role = Role.find_by_name("standard_user")
+	role.rights << Right.find_by_id(i)
+	role.save
+end
+
+[5, 6, 7, 8].each do |i|
+	role = Role.find_by_name("band")
+	role.rights << Right.find_by_id(i)
+	role.save
+end
+
+[1,2,3,4,5,6,7,8,9,11,12,13,14,15,16].each do |i|
+	role = Role.find_by_name("admin")
+	role.rights << Right.find_by_id(i)
+	role.save
+end
--- lib/tasks/roles_and_rights.rake
+++ lib/tasks/roles_and_rights.rake
@@ -1,8 +1,6 @@
 namespace :db do
 	desc "Add roles and associated rights"
 	task populate: :environment do
-		Role.create!(name: "standard_user")
-		Role.create!(name: "band")
-		Role.create!(name: "admin")
+		
 	end
 end
