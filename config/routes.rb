d1609610b64169a9f33afe1bd178dd2790f90e56
StevenButter <sbutter@dundee.ac.uk>
2012-12-04 12:58:43 +0000

    add terms and conditions

--- config/routes.rb
+++ config/routes.rb
@@ -31,6 +31,7 @@ ECommerceMusicShop::Application.routes.draw do
   match '/help'     => 'static_pages#help'
   match '/about'    => 'static_pages#about'
   match '/contact'  => 'static_pages#contact'
+  match '/terms'    => 'static_pages#terms'
 
   root :to => 'static_pages#home'
   # The priority is based upon order of creation:
